ad439399c6e49374c3823684d174be8bfa753d9d
BT_1N3_12 <wleung3409@bths.edu>
2018-10-04 08:33:58 -0400

    uyff

--- src/WilliamLeungProject/WilliamLeunglib.java
+++ src/WilliamLeungProject/WilliamLeunglib.java
@@ -1,4 +1,8 @@
 package WilliamLeungProject;
 
 public class WilliamLeunglib {
+    public static boolean palindrome(String input){
+        for (int i=0; i< input.length()/2; i++);
+        if (input.substring(i,i+1).equals) input.substring(input.length()-i,input.length());
+    }
 }
